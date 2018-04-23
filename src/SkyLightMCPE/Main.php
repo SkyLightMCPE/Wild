a69e91246f05c9da48409c1c8199040438d913d8
Ilhan Can Ates <ilhancan_ates@hotmail.com>
2018-04-23 21:30:36 +0300

    Idk why but it sometimes doesn't work

--- src/SkyLightMCPE/Main.php
+++ src/SkyLightMCPE/Main.php
@@ -5,15 +5,12 @@ namespace SkyLightMCPE;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\level\format\ChunkException;
 use pocketmine\math\Vector3;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase{
-
-    public $iswildin = [];
 
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
         if(strtolower($cmd->getName()) !== "wild") return false;
@@ -25,9 +22,8 @@ class Main extends PluginBase{
             $sender->sendMessage(TextFormat::RED . "You don't have permission to use this command.");
             return false;
         }
-        array_push($this->iswildin, $sender->getName());
-        $x = mt_rand(-999,999);
-        $z = mt_rand(-999,999);
+        $x = mt_rand(-256, 256);
+        $z = mt_rand(-256, 256);
         $sender->getLevel()->loadChunk($x, $z, true);
         $sender->teleport(new Vector3($x, $sender->getLevel()->getHighestBlockAt($x, $z) + 2, $z));
         $sender->sendMessage(TextFormat::RED . "Teleporting...");
